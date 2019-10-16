5347d0afce453ada5fb52534e6653ecab752f218
Roman Podymov <podymfrombryansk@yandex.ru>
2019-10-16 22:20:58 +0200

    Create MappingSetterTests.swift

---
+++ Tests/MappingSetterTests/MappingSetterTests.swift
@@ -0,0 +1,23 @@
+//
+//  MappingSetterTests.swift
+//  
+//
+//  Created by Roman Podymov on 16/10/2019.
+//
+import XCTest
+@testable import MappingSetter
+
+final class MappingSetterTests: XCTestCase {
+
+    override func setUp() {
+
+    }
+    
+    func testSomething() {
+
+    }
+    
+    static var allTests = [
+        ("testSomething", testSomething),
+    ]
+}
