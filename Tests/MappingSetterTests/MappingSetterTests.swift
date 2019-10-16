1df43a1870e5dfcf69929431fdd5c286795609be
Roman Podymov <podymfrombryansk@yandex.ru>
2019-10-16 23:04:20 +0200

    Update MappingSetterTests.swift

--- Tests/MappingSetterTests/MappingSetterTests.swift
+++ Tests/MappingSetterTests/MappingSetterTests.swift
@@ -4,20 +4,39 @@
 //
 //  Created by Roman Podymov on 16/10/2019.
 //
+
 import XCTest
 @testable import MappingSetter
 
 final class MappingSetterTests: XCTestCase {
 
-    override func setUp() {
+    @MappingSetter(mappingSetter: { $0.trimmingCharacters(in: .whitespacesAndNewlines) })
+    var trimmed: String = "   Hello, World! \n   \n"
 
-    }
-    
-    func testSomething() {
+    @MappingSetter(mappingSetter: { String($0.prefix(5)) })
+    var upToFive: String = "Hello, World!"
 
+    @MappingSetter(mappingSetter: { $0 })
+    var unmodified: String = "Hello, "
+
+    func testTrimmed() {
+        trimmed += "\n \n"
+        XCTAssertEqual(trimmed, "Hello, World!")
     }
-    
+
+    func testUpToFive() {
+        upToFive += "\n \n"
+        XCTAssertEqual(upToFive, "Hello")
+    }
+
+    func testUnmodified() {
+        unmodified += "World!"
+        XCTAssertEqual(unmodified, "Hello, World!")
+    }
+
     static var allTests = [
-        ("testSomething", testSomething),
+        ("testTrimmed", testTrimmed),
+        ("testUpToFive", testUpToFive),
+        ("testUnmodified", testUnmodified),
     ]
 }
