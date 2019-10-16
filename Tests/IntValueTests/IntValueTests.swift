0e207664170a428083943c35de115942546fc0ef
Roman Podymov <podymfrombryansk@yandex.ru>
2019-10-16 18:40:43 +0200

    Create IntValueTests.swift

---
+++ Tests/IntValueTests/IntValueTests.swift
@@ -0,0 +1,13 @@
+//
+//  IntValueTests.swift
+//  Burritos
+//
+//  Created by Roman Podymov on 16/10/2019.
+//
+
+@testable import IntValue
+import XCTest
+
+final class IntValueTests: XCTestCase {
+
+}
